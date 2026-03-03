3324777f8a822ff9f537f5bdcd817c5f333bc6ba
hanine007 <bouchibahanine@gmail.com>
2026-03-03 13:15:20 +0100

    add controller with post

--- src/main/java/com/example/demo/FirstController.java
+++ src/main/java/com/example/demo/FirstController.java
@@ -1,15 +1,32 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
 
 //to definiy is a controller rest 
 @RestController
 public class FirstController {
-    //get decorator
-@GetMapping("/")
-public String getMethodName() {
-    return "hello  from the Controller ";
+    private final JpaRepositoryy repositoryy;
+     
+    public FirstController(JpaRepositoryy repositoryy) {
+        this.repositoryy = repositoryy;
+    }
+ 
+//get decorator
+@PostMapping("/student")
+public String getMethodName(@RequestBody Student student) {
+    repositoryy.save(student);
+    return "hello  from the Controller this is the req  "+student.toString();
 }
 
+
 }
+
+
+
